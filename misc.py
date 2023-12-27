95846b10c8499bbc46a0e223bf731b489363491e
vzocca <vzocca@hotmail.com>
2023-12-27 12:32:03 -0500

    Update misc.py

--- misc.py
+++ misc.py
@@ -25,8 +25,6 @@ def clean_str(s):
         return result  # Output: 'Melaverde'
     else:
         return s
-    
-    
 
 def date_folder_create():
     today = datetime.now()
@@ -36,11 +34,35 @@ def date_folder_create():
     except (FileExistsError):
         os.mkdir(home + today.strftime('%Y-%m-%d') + '-bis')
     
-pattern = r'( N\.\d+ | N\. \d+ |_\d+|\d+|-)'    
-l = [re.split(pattern, f.split('-')[0])[0].strip() for f in os.listdir(home)]
+
+pattern = r'( N\.\d+ | N\. \d+ |_\d+|\d+|-| N\d+ | N \d+ )'
+
+pattern1 = r'( N\.\d+)'
+pattern2 = r'( N\. \d+)'
+pattern3 = r'( N\.\d+)'
+pattern4 = r'( N\. \d+)'
+pattern5 = r'(_\d+)'
+pattern6 = r'(\d+)'
+pattern7 = r'(-)'
+
+l = os.listdir(home)
+
+#l = ["La Civiltà Cattolica N.4163 - 2 Dicembre 2023.pdf"]
+
+l = [re.split(pattern1, f.split('-')[0])[0].strip() for f in l]
+l = [re.split(pattern2, f.split('-')[0])[0].strip() for f in l]
+l = [re.split(pattern3, f.split('-')[0])[0].strip() for f in l]
+l = [re.split(pattern4, f.split('-')[0])[0].strip() for f in l]
+l = [re.split(pattern5, f.split('-')[0])[0].strip() for f in l]
+l = [re.split(pattern6, f.split('-')[0])[0].strip() for f in l]
+l = [re.split(pattern7, f.split('-')[0])[0].strip() for f in l]
+
 l = [clean_str(f) for f in l]
 l = [f.split(".pdf")[0] for f in l]
 
 print(l)
+l = set(l)
 print(set(l) )
+
+len(l)
 
